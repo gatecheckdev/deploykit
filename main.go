e89cfd2cedf6d3838ef0590afdda6aa7069f32a1
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-04-11 14:58:28 -0500

    feat: add version command

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	cliVersion     = "[Not Provided]"
+	buildDate      = "[Not Provided]"
+	gitCommit      = "[Not Provided]"
+	gitDescription = "[Not Provided]"
 )
 
 func main() {
@@ -25,6 +34,35 @@ func newCommand() *cobra.Command {
 		Use:   "gtk",
 		Short: "GitOps Toolkit - A utility for common GitOps tasks",
 	}
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "version and build information",
+		Run:   runVersion,
+	}
+
 	cmd.SilenceUsage = true
+
+	cmd.AddCommand(versionCmd)
 	return cmd
+}
+
+func runVersion(cmd *cobra.Command, _ []string) {
+	_, _ = fmt.Fprintf(cmd.OutOrStdout(),
+		`CLIVersion:     %s
+GitCommit:      %s
+Build Date:     %s
+GitDescription: %s
+Platform:       %s/%s
+GoVersion:      %s
+Compiler:       %s
+`,
+		cliVersion,
+		gitCommit,
+		buildDate,
+		gitDescription,
+		runtime.GOOS,
+		runtime.GOARCH,
+		runtime.Version(),
+		runtime.Compiler,
+	)
 }
